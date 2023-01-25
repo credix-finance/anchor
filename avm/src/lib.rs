d74e1f8b7d2a5d3ad0711223eabae4c510f32372
Ruven Salamon <ruven.salamon@gmail.com>
2023-01-25 09:34:51 +0100

    replaces coral with own repo

--- avm/src/lib.rs
+++ avm/src/lib.rs
@@ -171,8 +171,8 @@ pub fn fetch_versions() -> Vec<semver::Version> {
 
     let client = reqwest::blocking::Client::new();
     let versions: Vec<Release> = client
-        .get("https://api.github.com/repos/coral-xyz/anchor/tags")
-        .header(USER_AGENT, "avm https://github.com/coral-xyz/anchor")
+        .get("https://api.github.com/repos/RuvenSalamon/anchor/tags")
+        .header(USER_AGENT, "avm https://github.com/RuvenSalamon/anchor")
         .send()
         .unwrap()
         .json()
